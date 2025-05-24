c58dc32b0824559e6ae34db5692aa3e1d7be8cbc
Swargam Saipranith <saipranith.s@CY-IND-L2136.local>
2025-05-24 13:29:11 +0530

    SEO more

--- cyware-website/next.config.js
+++ cyware-website/next.config.js
@@ -2,21 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/index.html',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/index.php',
-        destination: '/',
-        permanent: true,
-      }
-    ]
-  },
+  }
 }
 
 module.exports = nextConfig 
